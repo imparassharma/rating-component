1ebeff9942023ff57db2325ba8e451fd67c6a661
imparassharma <parasangiras121@gmail.com>
2023-11-26 21:18:17 +0530

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,26 @@
 import React from 'react';
 import { useState } from 'react';
 import Rating from "./Rating";
-import Button from './Button';
 import './App.css';
 import img1 from './images/icon-star.svg';
 
 
 function App(){
     
-    const [userRating, setUserRating] = useState<number | undefined>();
-    const handleRatingChange = (rating: number) => {
-      setUserRating(rating);
-      console.log(rating);
-    };
-    
+   
     return(
        
-       <div className="main">
-            <div className="logo ratingCircle"><img src={img1} /></div>
-            <h1>How did we do?</h1>
-            <p>Please let us know how we did with your support request. All feedback is appreciated to help us improve our offering!</p>
-            <Rating onRatingChange={handleRatingChange}/>
-            <Button text="SUBMIT"/>
+      <div className="mainSection">
+        <div className="main">
+            <div className="logo ratingCircle" id='logo'><img src={img1} /></div>
+            <h1 id='heading-text'>How did we do?</h1>
+            <p id='inner-text'>Please let us know how we did with your support request. All feedback is appreciated to help us improve our offering!</p>
+            <Rating/>
         </div>
+        <div className="warningContainer hidden" id='warning'>
+          <p>Please choose a rating!</p>
+        </div>
+      </div>
     )
 
 }
--- src/Rating.tsx
+++ src/Rating.tsx
@@ -1,15 +1,17 @@
 import React, { useState } from "react";
 
-interface RatingProps {
-    onRatingChange: (rating: number) => void;
-}
 
-function Rating({ onRatingChange }: RatingProps) {
+const Rating:React.FC=() =>{
       
     const [rate,setRate] = useState <number[]>([1,2,3,4,5]);
+    const [userRating, setUserRating] = useState<number>(0);
 
     const handleClick = (e:React.MouseEvent<HTMLElement>,id:number)=>{
         
+        setUserRating(id);
+        //hiding the warning message
+        const warning = document.getElementById('warning');
+        warning?.classList.add('hidden');
         //resetting color of previous rating if made
         rate.forEach((_, index) => {
             const element = document.getElementById(`${index}`);
@@ -25,15 +27,43 @@ function Rating({ onRatingChange }: RatingProps) {
             currentRating.style.color = 'white';
         }
 
-        onRatingChange(id+1);
-
+    }
+    
+    const handleSubmit = (e:React.MouseEvent<HTMLButtonElement>)=>{
+        if(userRating>0 && userRating<5)
+        {
+            const warning = document.getElementById('warning');
+            const ratingComponent = document.getElementById('rating-component');
+            ratingComponent?.classList.add('hidden');
+            const submitbtn = document.getElementById('submit-btn');
+            submitbtn?.classList.add("hidden");
+            const headtext = document.getElementById('heading-text');
+            if(headtext)
+            headtext.innerHTML = "Thank You!";
+            const innertext = document.getElementById('inner-text');
+            if(innertext)
+            innertext.innerHTML = 'We appriciate you taking the time to give a rating.If you ever need more support, don\'t hesitate to get in touch! '
+            const logo = document.getElementById('logo');
+            logo?.classList.add('hidden');
+            warning?.classList.add('hidden');
+        }
+        else{
+            const warning = document.getElementById('warning');
+            warning?.classList.remove('hidden');
+        }
     }
 
+
     return(
-        <div className="ratingComponent">
-            {rate.map((rating,index)=>(
-                <div id={`${index}`} className="ratingCircle" onClick={(e)=>handleClick(e,index)}>{rating}</div>
-            ))}
+        <div className="ratingSection">
+            <div className="ratingComponent" id="rating-component">
+                {rate.map((rating,index)=>(
+                    <div id={`${index}`} className="ratingCircle" onClick={(e)=>handleClick(e,index)}>{rating}</div>
+                ))}
+            </div>
+            <div className="submitBtn" id='submit-btn'>
+                <button  onClick={handleSubmit}>SUBMIT</button>
+            </div>
         </div>
         
     )
